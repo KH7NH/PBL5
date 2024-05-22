8b7fd87ba915adafd8843b1faf212403174f4021
KH7NH <duckhanh050620003@gmail.com>
2024-05-22 13:43:50 +0700

    Call API show board

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,8 +3,20 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { mockData } from '~/apis/mock-data'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    // Tạm thời fix cứng boardId, flow chuẩn chỉnh là chúng ta sử dụng react-router-dom để lấy chuẩn boardId từ URL về.
+    const boardId = '664b161c768689ce24639d0a'
+    // Call API
+    fetchBoardDetailsAPI(boardId).then(board => {
+      setBoard(board)
+    })
+  }, [])
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
